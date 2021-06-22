7877e679cbefb3e1a1ad03fc031e0d07a45f5850
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-22 17:33:24 +0530

    Added : Handle Null Exception if user provides Invalid Mood

--- test/MoodAnalyserTest.java
+++ test/MoodAnalyserTest.java
@@ -1,6 +1,6 @@
 /**
  * ************************** 
- * Purpose: Refactor the code to take the mood message in Constructor
+ * Purpose: Handle Exception if User Provides Invalid Mood
  *  
  * @author Piyush Shaw
  * @version 1.0
@@ -14,19 +14,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MoodAnalyserTest {
-	 @Test
-	    public void givenMessage_Whensad_shouldReturnSad()  {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser();
-	        String mood = moodAnalyser.analyseMood("I am in sad Mood");
+	@Test
+    public void givenMessage_Whensad_shouldReturnSad()  {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
 
-	        Assertions.assertEquals("SAD", mood);
-	    }
+        Assertions.assertEquals("SAD", mood);
+    }
+    @Test
+    public void givenMessage_Whensad_shouldReturnSad()  {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Any mood");
 
-	    @Test
-	    public void givenMessage_Whensad_shouldReturnSad()  {
-	        MoodAnalyser moodAnalyser = new MoodAnalyser();
-	        String mood = moodAnalyser.analyseMood("I am in any Mood");
+        Assertions.assertEquals("SAD", mood);
+    }
 
-	         Assertions.assertEquals("SAD", mood);
-	    }
+    @Test
+    public void givenMessage_Whensad_shouldReturnSad()  {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in happy mood");
+
+        Assertions.assertEquals("HAPPY", mood);
+    }
 }
