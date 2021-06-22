7ef2dcd509753ddf98429bedf9c939298e92ecd1
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-22 17:53:15 +0530

    Added :Given null message in constructor Throw Mood Analyser Exception

--- src/MoodAnalyser.java
+++ src/MoodAnalyser.java
@@ -1,19 +1,33 @@
 
+public enum ExceptionTypeClass{
+    NULLEXCEPTION("Mood cannot be null"); 
+    private String notice;
+
+    ExceptionTypeClass(String notice){
+        this.notice = notice;
+    }
+    public String getNotice(){
+        return this.notice;
+    }
+}
 public class MoodAnalyser {
-	 private String message;
-     public String MoodAnalyser(String message) {
-	        return this.message = message;
-	    }
+	private String message;
 
-	    public String analyseMood() {
-	        try {
-	            if (message.contains("sad"))
-	                return "SAD";
-	            else
-	                return "HAPPY";
+    public String MoodAnalyser(String message) {
+        return this.message = message;
+    }
 
-	        }catch (NullPointerException e){
-		        return "HAPPY";
-	        }
-	    }
+    public String analyseMood() {
+        try {
+            if (message.contains("sad"))
+                return "SAD";
+            else if (message.contains(""))
+                throw new MoodAnalyserException(ExceptionTypeClass.EMPTYEXCEPTION);
+            else
+                return "HAPPY";
+
+        }catch (NullPointerException e){
+	        throw new MoodAnalyserException(ExceptionTypeClass.NULLEXCEPTION);
+        }
+    }
 }
