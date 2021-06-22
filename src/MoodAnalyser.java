3afb6059b52f43d7242878dd075da646678396cc
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-22 17:06:14 +0530

    Added : This method returns Happy

--- src/MoodAnalyser.java
+++ src/MoodAnalyser.java
@@ -3,6 +3,7 @@ public class MoodAnalyser {
 	public String analyseMood(String message) {
         if (message.contains("sad"))
             return "SAD";
-		return message;
+        else
+            return "HAPPY";
     }
 }
